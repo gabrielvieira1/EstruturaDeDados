6d07afcca9f8a257a7d638937a1110722d65de28
Gabriel <Gabriel@DESKTOP-HPQK0N9>
2020-05-06 15:09:23 -0300

    Alteração scanner

--- EstruturaDeDados/src/EstruturaDeDados/Atividade07/Program.java
+++ EstruturaDeDados/src/EstruturaDeDados/Atividade07/Program.java
@@ -1,28 +1,46 @@
 package EstruturaDeDados.Atividade07;
 
+import java.util.Scanner;
+
 public class Program<T> {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		int[] vetor = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 		String[] vetorString = { "dario", "estrela", "fabio", "caio", "zelezin" };
-
-		ordenacaoString(alteraCase(vetorString));
-
-		for (String impVetor : vetorString) {
+		String[] vetorTeste = new String[5];
+		
+		for (int i = 0; i < vetorTeste.length; i++) {
+			System.out.println("Digite os nomes");
+			vetorTeste[i] = sc.next();
+		}
+		
+		ordenacaoString(alteraCase(vetorTeste));
+		//ordenacaoString(alteraCase(vetorString));
+		
+		for (String impVetor : vetorTeste) {
 			System.out.print(impVetor + ", ");
 		}
-		/*
-		 * for(int el: vetor) { System.out.print(el+", "); }
-		 */
+
+	/*	for (String impVetor : vetorString) {
+			System.out.print(impVetor + ", ");
+		}
+		*/
+		System.out.println();
+
+		for (int el : vetor) {
+			System.out.print(el + ", ");
+		}
+		
+		sc.close();
+
 	}
 
 	static void ordenacaoString(String[] v) {
 		int rep = 0;
 
 		for (int ult = v.length - 1; ult > 0; ult--) {
-
 			for (int i = 0; i < ult; i++) {
-
 				if (v[i].length() >= v[i + 1].length()) {
 					rep = v[i + 1].length();
 				} else if (v[i].length() < v[i + 1].length()) {
@@ -54,11 +72,22 @@ public class Program<T> {
 		return el;
 	}
 
-	/*
-	 * static void bolha(int[] v) { for (int ult = v.length - 1; ult>0; ult--) { for
-	 * (int i = 0; i < ult; i++) { if (v[i] > v[i+1]) { int aux = v[i]; v[i] =
-	 * v[i+1]; v[i+1] = aux; } } } }
-	 * 
-	 */
+	static void bolha(int[] v) {
+		for (int ult = v.length - 1; ult > 0; ult--) {
+			for (int i = 0; i < ult; i++) {
+				if (v[i] > v[i + 1]) {
+					int aux = v[i];
+					v[i] = v[i + 1];
+					v[i + 1] = aux;
+				}
+			}
+		}
+	}
+	
+	/*  Os metodos implementados utilizam o algoritmo BubbleSort para tipo inteiro e String
+	 * percorre a lista múltiplas vezes comparando todos os elementos adjacentes e troca de
+	 * lugar os que estiverem fora de ordem. A cada passagem, o maior elemento da lista vai
+	 * para o final dela.
+	 * */
 
 }
